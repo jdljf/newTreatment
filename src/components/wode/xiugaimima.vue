80785a2a6bf78dfd9ea2cde1e0e82b771a03b126
jdljf <1243270526@qq.com>
2019-01-06 15:51:42 +0800

    修改密码接口和输入校验

--- src/components/wode/xiugaimima.vue
+++ src/components/wode/xiugaimima.vue
@@ -5,44 +5,57 @@
       <p class="biaoti">修改密码</p>
     </div>
 
-    <!-- <form @submit.prevent="changePassword">
-      <input type="number" v-model="pasData.phoneNumber">
-      <input type="text" v-model="pasData.oldPassword" placeholder="请输入旧密码">
-      <input type="text" v-model="pasData.newPassword" placeholder="请输入密码">
-      <input type="text" v-model="pasData.repeatPassword" placeholder="请再次输入密码">
-      <input type="submit" value="确认修改">
-    </form> -->
     <div class="tijiao">
       <div class="yaoqiu">
         <span class="xinxi">手机</span>
         <div class="shuru">
-          <input type="text" v-model="pasData.phoneNumber" placeholder="请输入手机号" class="shurukuang">
+          <input
+            type="text"
+            v-model="changePas.phoneNumber"
+            placeholder="请输入手机号"
+            class="shurukuang"
+          >
         </div>
       </div>
       <div class="yaoqiu">
         <span class="xinxi">旧密码</span>
         <div class="shuru">
-          <input type="text" v-model="pasData.oldPassword" placeholder="请输入旧密码" class="shurukuang">
+          <input
+            type="text"
+            v-model="changePas.oldPassword"
+            placeholder="请输入旧密码"
+            class="shurukuang"
+          >
         </div>
       </div>
       <div class="yaoqiu">
         <span class="xinxi">新密码</span>
         <div class="shuru">
-          <input type="text" v-model="pasData.newPassword" placeholder="请输入新密码" class="shurukuang">
+          <input
+            type="text"
+            v-model="changePas.newPassword"
+            placeholder="请输入新密码"
+            class="shurukuang"
+          >
         </div>
       </div>
       <div class="yaoqiu">
         <span class="xinxi">重复</span>
         <div class="shuru">
-          <input type="text" v-model="pasData.repeatPassword" placeholder="请重复新密码" class="shurukuang">
+          <input
+            type="text"
+            v-model="changePas.repeatPassword"
+            placeholder="请重复新密码"
+            class="shurukuang"
+          >
         </div>
       </div>
 
-      <div @click.prevent="register" class="zhuceAnniu">注册</div>
+      <div @click.prevent="changePassword" class="zhuceAnniu">确认修改</div>
       <div class="tongyi">
         <span>忘记密码</span>
       </div>
-    </div>    
+    </div>
   </div>
 </template>
 
@@ -51,7 +64,7 @@ export default {
   name: "xiaoxi",
   data() {
     return {
-      pasData: {
+      changePas: {
         phoneNumber: "",
         oldPassword: "",
         newPassword: "",
@@ -60,26 +73,43 @@ export default {
     };
   },
   mounted() {
-      this.getPersonPhoneNum()
+    this.getPersonPhoneNum();
   },
   methods: {
-    getPersonPhoneNum(){
-        this.axios.post('/api/getPersonPhoneNum', {
-            name: 'bbb'
-        })
-        .then((res)=> {
-            if (res.data.err_code === 200) {
-                this.pasData.phoneNumber = res.data.phoneNumber
-            }
-        })
+    getPersonPhoneNum() {
+      this.axios.get("/api/getPersonPhoneNum").then(res => {
+        if (res.data.err_code === 200) {
+          this.changePas.phoneNumber = res.data.phoneNumber;
+        }
+      });
     },
     changePassword() {
-        this.axios.post('/api/changePassword', this.pasData)
-        .then((res)=> {
-            if (res.data.err_code === 200) {
-                alert('修改成功')
-            }
+      let reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,12}$/;
+      if (this.changePas.oldPassword.replace(/^\s+|\s+$/g, '').length <= 0) {
+        return alert("请输入旧密码");
+      }
+      if (
+        !reg.test(this.changePas.newPassword) ||
+        !reg.test(this.changePas.repeatPassword)
+      ) {
+        return alert("密码必须由6-12位数字加字母组成");
+      }
+      if (this.changePas.newPassword !== this.changePas.repeatPassword) {
+        return alert("密码必须相同");
+      }
+      this.axios
+        .post("/api/changePassword", {
+          phoneNumber: this.changePas.phoneNumber,
+          oldPassword: this.changePas.oldPassword,
+          newPassword: this.changePas.newPassword,
+          repeatPassword: this.changePas.repeatPassword
         })
+        .then(res => {
+          alert(res.data.message);
+          if (res.data.err_code === 200) {
+            
+          }
+        });
     },
     huitui() {
       if (this.$route.query.goindex === "true") {
@@ -188,7 +218,7 @@ export default {
           outline: none;
           border: 0 none;
         }
-        .xiangxi{
+        .xiangxi {
           text-align: right;
         }
       }
