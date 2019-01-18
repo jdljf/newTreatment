721d6683282def51464ba2552510e75f54171a99
jdljf <1243270526@qq.com>
2019-01-18 12:12:21 +0800

    我的消息下拉加载

--- src/components/wode/xiaoxi.vue
+++ src/components/wode/xiaoxi.vue
@@ -4,9 +4,13 @@
       <span class="iconfont icon-houtui icon" @click="huitui"></span>
       <p class="biaoti">我的消息</p>
     </div>
-
+    <div v-if="noData" class="noData">暂时没有数据</div>
     <ul class="liebiao">
-      <li @click="gotoMessageDetail(index)" v-for="(message, index) in message.message" class="liebiao-xiang">
+      <li
+        @click="gotoMessageDetail(index)"
+        v-for="(message, index) in message"
+        class="liebiao-xiang"
+      >
         <i class="tu iconfont icon-ling ling zuo"></i>
         <div class="zhong">
           <div class="biaoti">{{message_type(message.messageType)}}</div>
@@ -18,6 +22,16 @@
         </div>
       </li>
     </ul>
+
+    <div
+      v-infinite-scroll="loadMore"
+      infinite-scroll-disabled="busy"
+      infinite-scroll-distance="30"
+      v-if="!noData"
+    >
+      <div class="NewData">{{loadText}}</div>
+      <!-- <div v-show="noNewData">没有更多数据了</div> -->
+    </div>
   </div>
 </template>
 
@@ -26,41 +40,87 @@ export default {
   name: "xiaoxi",
   data() {
     return {
-      message: {}
+      checkedindex: "",
+      message: [],
+      pageNum: 1,
+      pageSize: 2,
+      busy: false,
+      noData: false,
+      noNewData: false,
+      loadText: "加载中"
     };
   },
-  computed:{
-    message_type(){
-      return function (opt) {
+  computed: {
+    message_type() {
+      return function(opt) {
         if (parseInt(opt) === 0) {
-          return '系统消息'
+          return "系统消息";
+        } else if (parseInt(opt) === 1) {
+          return "课程消息";
         }
-        else if (parseInt(opt) === 1) {
-          return '课程消息'
-        }
-      }
+      };
     }
   },
   mounted() {
-    this.getMyMessage()
+    this.getMoreSubject()
   },
   methods: {
-    getMyMessage() {
-      this.axios.get("/api/getMyMessage").then(res => {
-        this.message = res.data.message
-        console.log(res.data);
-        
+    loadMore: function() {
+      this.busy = true;
+      let that = this;
+      setTimeout(() => {
+        this.pageNum++;
+        this.getMoreSubject();
+        //  this.busy = false
+      }, 2000);
+    },
+    getMoreSubject() {
+      this.axios
+        .get("/api/getMyMessage", {
+          params: {
+            pageNum: this.pageNum,
+            pageSize: this.pageSize
+          }
+        })
+        .then(res => {
+          console.log(res.data);
+          if (this.pageNum == 1 && res.data.message <= 0) {
+            this.busy = true;
+            this.noData = true;
+          } else if (this.pageNum > 1 && res.data.message <= 0) {
+            (this.busy = true), 
+            (this.noNewData = true);
+            this.loadText = "没有更多数据了";
+            this.pageNum = 0;
+          } else {
+            this.busy = false;
+            this.message.push(...res.data.message);
+            console.log(this.message);
+            
+          }
+        });
+    },
+    // getMyMessage() {
+    //   this.axios.get("/api/getMyMessage").then(res => {
+    //     this.message = res.data.message
+    //     console.log(res.data);
+
+    //   });
+    // },
+    gotoMessageDetail(index) {
+      this.$router.push({
+        path: "/messageDetail",
+        query: {
+          messageId: this.message.message[index]._id,
+          message_type: this.message.message[index].messageType
+        }
       });
     },
-    gotoMessageDetail(index) {
-      this.$router.push({ path: "/messageDetail", query: { messageId: this.message.message[index]._id, message_type: this.message.message[index].messageType} });
-    },
-    huitui(){
-      if (this.$route.query.goindex === 'true') {
-        this.$router.push('/')
-      }
-      else {
-        this.$router.back(-1)
+    huitui() {
+      if (this.$route.query.goindex === "true") {
+        this.$router.push("/");
+      } else {
+        this.$router.back(-1);
       }
     }
   }
@@ -84,6 +144,10 @@ export default {
       text-align: center;
       margin: 0 1rem;
     }
+  }
+  .noData {
+    text-align: center;
+    margin-top: 50%;
   }
   .liebiao {
     padding: 0 0 0.15rem 0.2rem;
@@ -141,6 +205,12 @@ export default {
         }
       }
     }
+  }
+  .NewData {
+    text-align: center;
+    height: 0.6rem;
+    line-height: 0.6rem;
+    font-size: 0.26rem;
   }
 }
 </style>
